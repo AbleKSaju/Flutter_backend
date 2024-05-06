311bfcbf8fa165a6a058e1363817eaf15e316951
AbleKSaju <126228406+AbleKSaju@users.noreply.github.com>
2024-05-06 14:26:41 +0530

    Db error

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,9 +2,12 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
+        console.log(process.env.MONGOURL,"process.env.MONGOURL");
+        
         const conn = await mongoose.connect(process.env.MONGOURL ?? "");
         console.log(`MongoDB connected`);
     } catch (error) {
+        console.log(error);
         console.log(`MongoDb not connected`);
         process.exit(1);
     }
