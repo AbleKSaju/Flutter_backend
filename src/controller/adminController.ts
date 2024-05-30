dba3de10b0e0e39863acf94a4b0a29c61758d2d0
AbleKSaju <126228406+AbleKSaju@users.noreply.github.com>
2024-05-30 10:35:15 +0530

    status changed to 200

--- src/controller/adminController.ts
+++ src/controller/adminController.ts
@@ -53,7 +53,7 @@ export const addCategory = async (req: any, res: any) => {
 };
 
 export const getCategory = async (req: Request, res: Response) => {
-  await Category.find()
+  await Category.find({blocked:false})
     .lean()
     .then((data: any) => {
       if (data.length) {
@@ -167,7 +167,7 @@ export const addProduct = async (req: any, res: any) => {
 };
 
 export const getProduct = async (req: Request, res: Response) => {
-  await Product.find()
+  await Product.find({blocked:false})
     .lean()
     .then((data: any) => {
       if (data.length) {
